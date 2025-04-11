9a2673877d8a38501061bf993210095fd54b0a2f
Vince Joseph Ladrera <ladreravincejoseph@gmail.com>
2025-04-11 18:30:46 +0800

    Modify frontend template - Modify app.tsx file to fix return value

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -6,7 +6,6 @@ const router = createRouter({ routeTree });
 const queryClient = new QueryClient();
 
 function App() {
-	<></>;
 	return (
 		<QueryClientProvider client={queryClient}>
 			<RouterProvider router={router} />
